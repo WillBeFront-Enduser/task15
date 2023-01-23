12eab974a04dafacba79b0907fa0fd68efb9cc34
WillBeFront-Enduser <pugbmobile40@gmail.com>
2023-01-23 23:15:34 +0300

    yeahha

---
+++ script.js
@@ -0,0 +1,26 @@
+// Check if the user has a nickname stored in LocalStorage
+if (!localStorage.getItem("nickname")) {
+    // If not, generate a random nickname
+    localStorage.setItem("nickname", "User " + Math.floor(Math.random() * 1000));
+  }
+  
+  // Get the form, input, and display elements
+  const form = document.querySelector("form");
+  const input = document.querySelector("#message");
+  const display = document.querySelector("#display");
+  
+  // Listen for form submission
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    // Get the message and nickname
+    const message = input.value;
+    const nickname = localStorage.getItem("nickname");
+  
+    // Clear the input field
+    input.value = "";
+  
+    // Add the message to the display
+    display.innerHTML += `<p><strong>${nickname}:</strong> ${message}</p>`;
+  });
+  
